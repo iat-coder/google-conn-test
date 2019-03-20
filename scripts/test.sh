22e03161b149bacbfa075ef090ae885360724fa0
Igor Talzi <igor.talzi@gmail.com>
2019-03-20 08:39:26 +0000

    scripts: add more test cases

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 ../bin/gconn_app -H "User-Agent: gconn" -H "X-MyHeader: 123" -n -5 -i 1000
+../bin/gconn_app -H "X-MyHeader: 456" -n 25
 ../bin/gconn_app -n -45 -i -100
+../bin/gconn_app -n -45 -i -100 2>/dev/null
+../bin/gconn_app
